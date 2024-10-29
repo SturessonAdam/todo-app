6ac2e99a5bda9a5273804210b431a8a594b930fe
Adam <Adam.sturesson@live.se>
2024-10-29 15:16:29 +0100

    test toggle todo

--- src/components/TodoWrapper.cy.tsx
+++ src/components/TodoWrapper.cy.tsx
@@ -19,8 +19,16 @@ describe('<TodoWrapper />', () => {
   it('adds a new todo to the list', () => {
     cy.get('input[placeholder="Add a new todo"]').type('new todo');
     cy.get('button[type="submit"]').click();
-
     cy.contains('new todo').should('be.visible');
+  })
+
+  it('toggles the todo to be completed (green)' , () => {
+    cy.get('input[placeholder="Add a new todo"]').type('new todo to toggle');
+    cy.get('button[type="submit"]').click();
+    cy.contains('new todo to toggle').should('be.visible');
+
+    cy.contains('new todo to toggle').click();
+    cy.contains('new todo to toggle').should('have.class', 'completed');
   })
 
 })
